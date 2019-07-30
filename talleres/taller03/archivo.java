3451cfe643ccd50c822c2cee3cf39413a561d51f
Liz Oriana Rodrigues Cruz <52968501+LizOriana1409@users.noreply.github.com>
2019-07-30 11:45:14 -0500

    Update archivo.java

--- talleres/taller03/archivo.java
+++ talleres/taller03/archivo.java
@@ -3,6 +3,30 @@
  * @author juansedo, LizOriana1409
  */
 public class Taller3 {
+	/**
+    *
+    * Ejercicio 1.
+    */
+     public static void TorresPrueba() {
+          int n = 4; //Se puede cambiar a gusto.
+          Torres(n, 1, 3, 2);
+     }
+
+     public static void Torres(int n, int origen, int destino, int auxiliar) {
+          if (n == 1) {
+               System.out.println("Mover de " + origen + " a " + destino);
+          } else {
+               Torres(n - 1, origen, auxiliar, destino);
+               System.out.println("Mover de " + origen + " a " + destino);
+               Torres(n - 1, auxiliar,destino, origen);
+               
+          }
+     }
+    
+    /**
+    *
+    * Ejercicio 2. 
+    */
     public static void permute(String a, String output) {
         for (int i = 0; i < a.length(); i++) {
             char c = a.charAt(i);
