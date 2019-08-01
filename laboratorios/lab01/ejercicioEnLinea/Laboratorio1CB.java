c9aee35c8c7a612359e00c99492f0e1bec1149cb
Liz Oriana Rodrigues Cruz <52968501+LizOriana1409@users.noreply.github.com>
2019-08-01 09:36:37 -0500

    Update Laboratorio1CB.java

--- laboratorios/lab01/ejercicioEnLinea/Laboratorio1CB.java
+++ laboratorios/lab01/ejercicioEnLinea/Laboratorio1CB.java
@@ -49,6 +49,40 @@ public class Laboratorio1CB {
 	}
 	
 	/**
+     * parenBit 
+     * Recursion-1 CodingBat
+     */
+    public String parenBit(String str) {
+        if (str.equals("")) {
+            return str;
+        } 
+        if (str.charAt(0) == '(') {
+            if (str.charAt(str.length()-1) == ')'){
+                return str;
+            }else{
+                return parenBit(str.substring(0, str.length()-1));
+            }
+        } else {
+            return parenBit(str.substring(1)); }
+    }
+
+    /**
+     * nestParen 
+     * Recursion-1 CodingBat
+     */
+    public boolean nestParen (String str){
+        int x = str.length();
+        if (x == 0){
+            return true;
+        } 
+        if (str.charAt(0) == '(' && str.charAt(x -1) == ')'){
+            return nestParen(str.substring(1, x-1));
+        } else {
+            return false;
+        }
+    }
+	
+	/**
 	* Recursión 2: groupSum6
 	* Devuelve true si se puede obtener target al sumar
 	* algunos números de nums. Con la condición de que,
@@ -78,4 +112,63 @@ public class Laboratorio1CB {
 			groupNoAdj(start + 2, nums, target - nums[start])
 			|| groupNoAdj(start + 1, nums, target);
 	}
+	
+	/**
+     * groupSumClump 
+     * Recursion-2 CodingBat
+     */
+    public boolean groupSumClump(int start, int[] nums, int target) {
+        altArray(nums);
+        if (start >= nums.length) {
+            return target == 0;}
+        if (groupSumClump(start+1, nums, target-nums[start])) {
+            return true; }
+        if (groupSumClump(start+1, nums, target)) {
+            return true;}
+        else {
+            return false;} 
+    }
+    private void altArray(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                nums[i-1] += nums[i];
+                if (i+1 < nums.length && nums[i] != nums[i+1])
+                    nums[i] = 0;
+                else if (i == nums.length-1)
+                    nums[i] = 0;
+            }
+        } 
+    }
+
+    /**
+     * splitOdd10 
+     * Recursion-2 CodingBat
+     */
+    public boolean splitOdd10(int[] nums) {
+        return helper(0, nums, 0, 0);
+    }
+    //Se crea una clase de apoyo para realizar el metodo recursivo 
+    private boolean helper(int start, int[] nums, int sum1, int sum2) {
+        if (start >= nums.length)
+            return sum1 % 10 == 0 && sum2 % 2 == 1 || sum1 % 2 == 1 && sum2 % 10 == 0;
+        return helper(start + 1, nums, sum1 + nums[start], sum2)|| helper(start + 1, nums, sum1, sum2 + nums[start]);
+    }
+
+    /**
+     * splitArray 
+     * Recursion-2 CodingBat
+     */
+    public boolean splitArray(int[] nums) {
+        return Help (nums, 0, 0);   
+    }
+    //Se crea una clase de apoyo para realizar el metodo recursivo 
+    public boolean Help (int[] nums, int i, int balance) {
+        if(i == nums.length) {
+            return (balance == 0);
+        }
+        if(Help (nums, i + 1, balance + nums[i])) {
+            return true; 
+        }
+        return Help (nums, i + 1, balance - nums[i]);
+    }
 }
