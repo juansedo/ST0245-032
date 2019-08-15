4b786813a7852c236a06e28bf56a2bef87770070
Juan Sebastián Díaz Osorio <52968530+juansedo@users.noreply.github.com>
2019-08-14 20:44:18 -0500

    Update archivo.java

--- talleres/taller05/archivo.java
+++ talleres/taller05/archivo.java
@@ -1,7 +1,11 @@
 class Taller5 {
-  
-  //Forma por ciclos
-  // O(n^2)
+          //Insertion Sort
+  /** Ciclos
+  * Organiza por medio del insertion sort un arreglo.
+  * La complejidad de este código está dada por O(n^2),
+  * donde n representa la longitud del string,
+  * ya que usa dos ciclos anidados.
+  */
   public static int [] sort(int [] arr) {
     for (int i = 1; i < arr.length; i++) {
       if (arr[i - 1] > arr[i]) {
@@ -18,13 +22,31 @@ class Taller5 {
     return arr;
   }
   
-  //Forma por recursión
-  //O(n^2)
-  public static int [] sort(int [] arr, int start) {
-    if (arr.length == start) return arr;
-    return sort(insert(arr, start), start + 1);
+  /** Recursión
+  * Organiza por medio del insertion sort un arreglo.
+  * La complejidad de este código está dada por la ecuación:
+  * T(n) = F(n) + T(n-1) + C3
+  * donde F(n) hace referencia al método recursivo insert() y
+  * n a la longitud de la cadena.
+  *
+  * Operándose en Wolfram resulta:
+  * T(n) = C1*n + C2 + C3*n + (C4*n)(n+1)/2
+  * Que es O(n^2).
+  */
+  public static int [] rsort(int [] arr) {
+    return rsort(arr, 1);
   }
-
+  
+  public static int [] rsort(int [] arr, int start) {
+    if (arr.length == start) return arr;
+    return rsort(insert(arr, start), start + 1);
+  }
+  
+  /**
+  * Complejidad:
+  * F(n) = F(n - 1) + C4, que es O(n)
+  * Donde n es la longitud del arreglo.
+  */
   private static int [] insert(int [] arr, int start) {
     if (start == 0) return arr;
     
