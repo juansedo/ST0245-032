a20d2d919afdd25b1e316681682a5a493b1801d0
Juan Sebastián Díaz Osorio <52968530+juansedo@users.noreply.github.com>
2019-08-15 15:30:52 -0500

    Update Laboratorio1CB.java

--- laboratorios/lab01/ejercicioEnLinea/Laboratorio1CB.java
+++ laboratorios/lab01/ejercicioEnLinea/Laboratorio1CB.java
@@ -159,8 +159,8 @@ public class Laboratorio1CB {
     
 	//Se crea una clase de apoyo para realizar el metodo recursivo 
     private boolean helper(int start, int[] nums, int sum1, int sum2) {
-        if (start >= nums.length) return (sum1 % 2 == 0 && sum2 % 2 == 1)
-		|| (sum1 % 2 == 1 && sum2 % 2 == 0);
+        if (start >= nums.length) return (sum1 % 10 == 0 && sum2 % 2 == 1)
+		|| (sum1 % 2 == 1 && sum2 % 10 == 0);
         return helper(start + 1, nums, sum1 + nums[start], sum2) 
 		|| helper(start + 1, nums, sum1, sum2 + nums[start]);
     }
