d2f1afa680034813acf7a14e3b0a9a6d7bf78a03
Juan Sebastián Díaz Osorio <52968530+juansedo@users.noreply.github.com>
2019-08-27 15:05:57 -0500

    Update CodingBat.java

--- laboratorios/lab02/ejercicioEnLinea/CodingBat.java
+++ laboratorios/lab02/ejercicioEnLinea/CodingBat.java
@@ -3,39 +3,17 @@
 *
 * @author juansedo, LizOriana1409
 */
-class CodingBat 
-
+class CodingBat {
+    
             //Array-2
-    
-{public int countEvens(int[] nums) { 
-    int cont = 0; 
-    for (int i = 0; i < nums.length; i++) 
-        if (nums[i] % 2 == 0) cont++; 
-    return cont; 
-} 
-
-public int bigDiff(int[] nums) { 
-  int max = nums[0]; 
-  int min = nums[0]; 
-  for (int i = 0; i < nums.length; i++) { 
-    if (nums[i] > max) max = nums[i];   
-    if (nums[i] <= min) min = nums[i]; 
-  } 
-  return max - min; 
-} 
- 
-public int centeredAverage(int[] nums) { 
-    int max = nums[0]; 
-    int min = nums[0]; 
-    int sum = 0; 
-    for (int i = 0; i < nums.length; i++) { 
-        sum += nums[i]; 
-        if (nums[i] > max) max = nums[i]; 
-        if (nums[i] < min) min = nums[i]; 
-    } 
-  return (sum - (max + min)) / (nums.length - 2); 
-} 
-    
+    /**
+    * Suma los elementos de un arreglo, evitando los 13
+    * y el número siguiente a este.
+    *
+    * Complejidad asintótica:
+    * T(n) = n + C_1
+    * Esto es O(n)
+    */
     public int sum13(int[] nums) {
         int sum = 0;
         for(int i = 0; i < nums.length; i++) {
@@ -44,7 +22,15 @@ public int centeredAverage(int[] nums) {
         }
         return sum;
     }
-       
+    
+    /**
+    * Suma los elementos de un arreglo, evitando los números
+    * entre el primer 6 y el primer 7.
+    *
+    * Complejidad asintótica:
+    * T(n) = n + C_1
+    * Esto es O(n)
+    */
     public int sum67(int[] nums) {
         int sum = 0;
         boolean able = true;
@@ -57,32 +43,14 @@ public int centeredAverage(int[] nums) {
     }
     
             //Array-3
-    
- public int maxSpan(int[] nums) { 
-    if (nums.length > 0) { 
-        int n = 1; 
-        for (int i = 0; i < nums.length; i++) 
-            for (int j = nums.length - 1; j > i; j--) 
-                if (nums[j] == nums[i]) { 
-                    int cont = (j - i) + 1; 
-                    if (cont > n) n = cont; 
-                    break; 
-                } 
-        return n; 
-    } else return 0; 
-} 
- 
-public int[] fix34(int[] nums) { 
-    for (int i = 0; i < nums.length; i++) 
-        if (nums[i] == 3) { 
-            int x = nums[i + 1]; 
-            nums[i + 1] = 4; 
-            for (int j = i + 2; j < nums.length; j++) 
-                if (nums[j] == 4) nums[j] = x; 
-        } 
-    return nums; 
-} 
- 
+    /**
+    * Reordena un arreglo haciendo que a cada 4
+    * le siga un 5.
+    *
+    * Complejidad asintótica:
+    * T(n) = n^2 + C_1
+    * Esto es O(n^2)
+    */
     public int[] fix45(int[] nums) {
         int i = 0;
         while(i + 1 < nums.length) {
@@ -105,6 +73,14 @@ public int[] fix34(int[] nums) {
         return nums;
     }
     
+    /**
+    * Divide un arreglo en dos subgrupos. Retorna true si hay 
+    * una forma de partir el arreglo tal que ambos lados sumen igual.
+    *
+    * Complejidad asintótica:
+    * T(n) = n^2 + C_1
+    * Esto es O(n^2)
+    */
     public boolean canBalance(int[] nums) {
         int sum1 = 0;
         int sum2 = 0;
@@ -119,6 +95,14 @@ public int[] fix34(int[] nums) {
         return false;
     }
     
+    /**
+    * Dados dos arreglos, retorna true si todos los elementos del segundo 
+    * arreglo están en el primero.
+    *
+    * Complejidad asintótica:
+    * T(n) = n^2 + C_1
+    * Esto es O(n^2)
+    */
     public boolean linearIn(int[] outer, int[] inner) {
         int j = 0;
         for(int i = 0; i < outer.length && j < inner.length; i++) {
