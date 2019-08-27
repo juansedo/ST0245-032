94d79beb64b8cb9ef86a22e2b08b2d154fb0cbc0
Liz Oriana Rodrigues Cruz <52968501+LizOriana1409@users.noreply.github.com>
2019-08-27 11:47:00 -0500

    Update Taller7.java

--- talleres/taller07/Taller7.java
+++ talleres/taller07/Taller7.java
@@ -4,23 +4,39 @@
  */
 public class LinkedList {
     private int size;
+    
+    /**
+    * Referencia al nodo inicial
+    */
     public Node startNode;
     
+    /**
+    * Método constructor.
+    */
     public LinkedList() {
         startNode = null;
         size = 0;
     }
     
+    /**
+    * @return devuelve el tamaño de la lista
+    */
     public int Size() {
         return size;
     }
     
+    /**
+    * Devuelve el valor en la posición pos de la lista.
+    * @param pos posición en la lista.
+    * @return valor en pos.
+    */
     public int get(int pos) {
-        if (pos > size) return get(size-2);
+        /*Si el tamaño es mayor, devuelve el */
+        if (pos > size) return get(size-1);
         if (pos < 0) return get(0);
         
         Node m = startNode;
-        
+        /*Buscamos el elemento en pos*/
         for (int i = 0; i < size; i++) {
             if (i == pos) break;
             m = m.next;
@@ -28,18 +44,32 @@ public class LinkedList {
         return m.getValue();
     }
     
+    /**
+    * Inserta el valor dado en la posición pos de la lista.
+    * @param pos posición en la lista.
+    * @param value valor a agregar.
+    */
     public void insert(int pos, int value) {
+        /*Si la posición es menor que 0, devuelve el primer valor*/
         if (pos < 0) {
             insert(0, value);
             return;
         }
         
-        size++;
         if (pos == 0) insertBeginning(value);
-        else if (pos < size) insertInside(startNode, pos - 1, value);
+        
+        else if (pos < size) {
+            size++;
+            insertInside(startNode, pos - 1, value);
+        }
+        
         else insertAfter(value);
     }
     
+    /**
+    * Inserta el valor dado al principio de la lista.
+    * @param value valor a agregar.
+    */
     public void insertBeginning(int value) {
         Node n = new Node(value);
         n.next = startNode;
@@ -48,6 +78,7 @@ public class LinkedList {
     }
     
     private void insertInside(Node n, int pos, int value) {
+        /*Este método recorre recursivamente los nodos de la lista*/
         if (pos != 0) insertInside(n.next, pos - 1, value);
         else {
             Node m = new Node(value);
@@ -56,11 +87,17 @@ public class LinkedList {
         }
     }
     
+    /**
+    * Inserta el valor dado al final de la lista.
+    * @param value valor a agregar.
+    */
     public void insertAfter(int value) {
+        size++;
         insertAfter(startNode, value);
     }
     
     private void insertAfter(Node n, int value) {
+        /*Este método recorre recursivamente los nodos*/
         if (n.next != null) insertAfter(n.next, value);
         else {
             Node m = new Node(value);
@@ -81,14 +118,27 @@ public class LinkedList {
     }
     
     private String toString(Node n) {
+        /*Devuelve:
+        * [e1, e2, e3]
+        * para una lista de 3 elementos*/
         return n.getValue() + ((n.next != null)? ", " + toString(n.next): "]");
     }
 }
 
+/**
+ * Clase que representa un nodo
+ * @author juansedo, LizOriana1409
+ */
 class Node {
     private int value;
+    /**
+    * Nodo enlazado
+    */
     public Node next;
     
+    /**
+    *
+    */
     public Node(int i) {
         value = i;
     }
