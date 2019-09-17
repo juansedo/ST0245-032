66fcf76806a1be85c33b43f1986263f38ea1aadd
Juan Sebastián Díaz Osorio <52968530+juansedo@users.noreply.github.com>
2019-09-17 11:43:08 -0500

    Update taller9.java

--- talleres/taller09/taller9.java
+++ talleres/taller09/taller9.java
@@ -39,3 +39,45 @@ public class TablaHash {
         tabla[funcionHash(k)] = v;
     }
 }
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author juansedo, LizOriana1409
+ */
+class PuntosAdicionales {
+    public static void main(String [] args) {
+        /*PUNTO 2*/
+        
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Google", "Estados Unidos");
+        map.put("La locura", "Colombia");
+        map.put("Nokia", "Finlandia");
+        map.put("Sony", "Japón");
+        
+        /*PUNTO 3*/
+        search_business(map, "Google");
+        search_business(map, "Motorola");
+        
+        /*PUNTO 4*/
+        System.out.println("Empresa en India: ");
+        System.out.println(search_country(map, "India"));
+        System.out.println("Empresa en Estados Unidos: ");
+        System.out.println(search_country(map, "Estados Unidos"));
+    }
+    
+    public static void search_business(HashMap<String, String> map, String business) {
+        String str = map.get(business);
+        if (str == null) System.out.println("No se encuentran datos de " + business + ".");
+        else System.out.println(business + " está en " + str);
+    }
+    
+    public static String search_country(HashMap<String, String> map, String country) {
+        for (String s : map.keySet()) {
+            if (country.equals(map.get(s))) return s;
+        }
+        return "";
+    }
+}
+
