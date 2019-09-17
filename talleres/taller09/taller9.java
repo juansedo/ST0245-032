ac11407db69eaf1276c81cb7d5945ba593b56dde
Juan Sebastián Díaz Osorio <52968530+juansedo@users.noreply.github.com>
2019-09-17 11:06:15 -0500

    Update taller9.java

--- talleres/taller09/taller9.java
+++ talleres/taller09/taller9.java
@@ -1,36 +1,41 @@
 /**
-* Clase que representa una tabla hash con 10 elementos como máximo
-* @author juansedo, LizOriana1409
-*/
+ * Clase que representa una tabla hash con 10 elementos como máximo
+ *
+ * @author juansedo, LizOriana1409
+ */
 public class TablaHash {
-   private int[] tabla;
-	 
-	 public TablaHash() {
-	 		tabla = new int[10];
-   }
 
-   /**
-   * @param k llave o key de la tabla
-   */
-   private int funcionHash(String k){
-       return ((int) k.charAt(0)) % 10;
-   }
-
-   /**
-   * @param k es la llave del elemento que servirá como referencia al buscar
-   * Este método se utiliza para obtener el elmento con la clave k, se enlaza con funcionHash
-   */
-   public int get(String k) {
-	 		return tabla[funcionHash(k)];
-   }
-
-   /**
-   * @param k es la llave del elemento que servirá como referencia al buscar posteriormente
-   * @param v es el valor asociado a la llave k
-   * Este método se utiliza para agregar un nuevo elemento
-   */
-   public void put(String k, int v){
-	 		int a = funcionHash(k);
-			tabla[a] = v;
-   }
+    private int[] tabla;
+    
+    public TablaHash() {
+        tabla = new int[10];
+    }
+    
+    /**
+     * @param k Nombre de la persona
+     * @return Posición en la que se va a ubicar
+     */
+    private int funcionHash(String k) {
+        int num = 0;
+        for (int i = 0; i < k.length(); i++) {
+            num += Math.pow( ((int)k.charAt(i))*i, 32-i);
+        }
+        return num % 10;
+    }
+    
+    /**
+     * @param k Nombre de la persona
+     * @return Número de teléfono
+     */
+    public int get(String k) {
+        return tabla[funcionHash(k)];
+    }
+    
+    /**
+     * @param k Nombre de la persona
+     * @param v Número de teléfono
+     */
+    public void put(String k, int v) {
+        tabla[funcionHash(k)] = v;
+    }
 }
