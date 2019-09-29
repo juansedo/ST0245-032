9370073c1fae729985fdd1381b076b157ff9b723
Juan Sebastián Díaz Osorio <52968530+juansedo@users.noreply.github.com>
2019-09-29 02:04:38 -0500

    Versión estable
    Hace falta comentar el código, pero puede probarse junto con el archivo Main.java

--- proyecto/codigo/Data.java
+++ proyecto/codigo/Data.java
@@ -8,26 +8,16 @@ import java.util.Scanner;
  *
  * @author juansedo, LizOriana1409
  */
-public class Data {
+public final class Data {
 
-    private double ph;
-    private double sTemperature;
-    private double sMoisture;
-    private double illuminance;
-    private double eTemperature;
-    private double eHumidity;
+    private final double ph;
+    private final double sTemperature;
+    private final double sMoisture;
+    private final double illuminance;
+    private final double eTemperature;
+    private final double eHumidity;
     private boolean label;
 
-    public Data() {
-        ph = 0;
-        sTemperature = 0;
-        sMoisture = 0;
-        illuminance = 0;
-        eTemperature = 0;
-        eHumidity = 0;
-        label = false;
-    }
-    
     public Data(double ph, double sTemp, double sMois, double illum, double eTemp, double eHum, boolean label) {
         this.ph = ph;
         sTemperature = sTemp;
@@ -42,56 +32,41 @@ public class Data {
         return ph;
     }
 
-    public void setPh(double ph) {
-        this.ph = ph;
-    }
-
     public double getsTemperature() {
         return sTemperature;
-    }
-
-    public void setsTemperature(double sTemperature) {
-        this.sTemperature = sTemperature;
     }
 
     public double getsMoisture() {
         return sMoisture;
     }
 
-    public void setsMoisture(double sMoisture) {
-        this.sMoisture = sMoisture;
-    }
-
     public double getIlluminance() {
         return illuminance;
-    }
-
-    public void setIlluminance(double illuminance) {
-        this.illuminance = illuminance;
     }
 
     public double geteTemperature() {
         return eTemperature;
     }
 
-    public void seteTemperature(double eTemperature) {
-        this.eTemperature = eTemperature;
-    }
-
     public double geteHumidity() {
         return eHumidity;
-    }
-
-    public void seteHumidity(double eHumidity) {
-        this.eHumidity = eHumidity;
     }
 
     public boolean getLabel() {
         return label;
     }
-
-    public void setLabel(boolean label) {
-        this.label = label;
+    
+    public void setLabel(Dataset ds) throws Exception {
+        if(ds.getRoot() != null) setLabel(ds, ds.getRoot());
+        else System.out.println("Dataset no considerado de ejemplo");
+    }
+    
+    public void setLabel(Dataset ds, Node n) throws Exception {
+        if(n.yes == null) this.label = n.answer;
+        else {
+            Node op = (n.compare(this.getValue(n.reason)))? n.yes: n.no;
+            setLabel(ds, op);
+        }
     }
     
     public double getValue(DataType t) throws Exception {
@@ -114,27 +89,29 @@ public class Data {
     }
 }
 
-class Dataset {
+final class Dataset {
 
     private LinkedList<Data> data;
-    Node rootDT;
-
+    private Node rootDT;
+    
     public Dataset(File f, boolean example) throws FileNotFoundException {
         Scanner in = new Scanner(f);
-        
         data = new LinkedList<>();
+        
+        //Ignora la primera linea, donde está la descripción de cada columna
         in.nextLine();
         
         while (in.hasNextLine()) {
             addData(in.nextLine());
         }
+        
+        //Si es Dataset de entrenamiento, se crea el árbol
         if (example) {
-            rootDT = new Node();
             rootDT = createTree(data);
         }
     }
     
-    public final Data addData (String toSplit) {
+    public Data addData (String toSplit) {
         String[] str = toSplit.split(",");
         try {
             Data d = new Data(Double.parseDouble(str[0]), Double.parseDouble(str[1]),
@@ -145,33 +122,35 @@ class Dataset {
             return d;
         }
         catch (IndexOutOfBoundsException e) {
-            System.out.println("Error agregando -> " + str);
+            System.out.println("Error agregando -> " + toSplit);
             System.out.println("Problema con formato y comas.");
             return null;
         }
     }
-
-    public final Data addData(Data d) {
-        data.add(d);
-        return d;
-    }
-
+    
     private Node createTree(LinkedList<Data> dataset) {
         
+        //Un único elemento define el nodo
         if(dataset.size() == 1) {
-            return new Node(data.getFirst().getLabel());
+            return new Node(dataset.getFirst().getLabel());
         }
         
         try {
+            //Sin elementos, arrojamos nulo
             if(dataset.isEmpty()) throw new Exception("Dataset vacío");
             double entropy_g = Gain.generalEntropy(dataset);
-            if (entropy_g == 0) return new Node(data.getFirst().getLabel());
             
+            //Entropía = 0 representa homogeneidad en los datos
+            if (entropy_g == 0) return new Node(dataset.getFirst().getLabel());
+            
+            //Se buscan el mínimo de entropía en los datos y
+            //el valor del nodo y el tipo de dato que lo logran
             double min_ent = Double.MAX_VALUE;
             double node_value = Double.MAX_VALUE;
             DataType sel_type = DataType.PH;
             
             for(DataType t : DataType.values()) {
+                //test[0] -> Mínimo de entropia, test[1] -> Valor con el que se logra
                 double [] test = Gain.partialEntropy(dataset, t);
                 if (test[0] < min_ent) {
                     min_ent = test[0];
@@ -180,18 +159,17 @@ class Dataset {
                 }
             }
             
-            System.out.println(min_ent);
-            
             //Nodo creado
             Node n = new Node(node_value, sel_type);
             
-            //Crear las ramas
+            //Se crean los subconjuntos
             LinkedList<Data> s1 = new LinkedList<>(), s2 = new LinkedList<>();
             for (Data d : dataset) {
                 if (n.compare(d.getValue(sel_type))) s1.add(d);
                 else s2.add(d);
             }
             
+            //Se crean los nodos hijos
             Node m = createTree(s1);
             n.yes = (m != null)? m: new Node(true);
             
@@ -199,57 +177,22 @@ class Dataset {
             n.no = (m != null)? m: new Node(false);
             
             return n;
-            
-            /*
-            testGain.setPh(entropy_g - Gain.partialEntropy(dataset, DataType.PH));
-            testGain.setsTemperature(entropy_g - Gain.partialEntropy(dataset, DataType.STEMP));
-            testGain.setsMoisture(entropy_g - Gain.partialEntropy(dataset, DataType.SMOIS));
-            testGain.setIlluminance(entropy_g - Gain.partialEntropy(dataset, DataType.ILLUM));
-            testGain.seteTemperature(entropy_g - Gain.partialEntropy(dataset, DataType.ETEMP));
-            testGain.seteHumidity(entropy_g - Gain.partialEntropy(dataset, DataType.EHUM));
-            */
-            
-            
-            /*
-            System.out.println("PH: " + testGain.getPh());
-            System.out.println("STEMP: " + testGain.getsTemperature());
-            System.out.println("SMOIS: " + testGain.getsMoisture());
-            System.out.println("ILLUM: " + testGain.getIlluminance());
-            System.out.println("ETEMP: " + testGain.geteTemperature());
-            System.out.println("EHUM: " + testGain.geteHumidity());
-            */
-            
-            
-            
         }
         catch(Exception e) {
-            System.out.println(e.getMessage());
             return null;
         }
     }
     
-    @Deprecated
-    public double MinValue(DataType t) throws Exception {
-        double min = Double.MAX_VALUE;
-        for (Data d : this.data) {
-            double test = d.getValue(t);
-            min = (test < min)? test: min;
-        }
-        return min;
+    public Node getRoot() {
+        return rootDT;
     }
 }
 
 class Node {
-    
     public DataType reason;
     public double value;
     public boolean answer;
-    public Node yes;
-    public Node no;
-
-    public Node() {
-        value = 0;
-    }
+    public Node yes, no;
 
     public Node(double value, DataType t) {
         this.value = value;
@@ -266,14 +209,15 @@ class Node {
 }
 
 class Gain {
-    
     static double generalEntropy(LinkedList<Data> dataset) throws Exception {
         int S_total = dataset.size();
         
+        //Sin datos no se calcula entropía
         if(S_total == 0) throw new Exception("Conjunto vacío");
         
         double yes = 0, no = 0;
         
+        //Se averigua cantidad de yes y no
         for (Data d : dataset) {
             if (d.getLabel()) yes++;
             else no++;
