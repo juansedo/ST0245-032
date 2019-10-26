5c8e19e313fc584789402fb3748167f67df93158
Juan Sebastián Díaz Osorio <52968530+juansedo@users.noreply.github.com>
2019-10-26 12:09:41 -0500

    Update Main.java

--- proyecto/codigo/Snd_DataStructure/Main.java
+++ proyecto/codigo/Snd_DataStructure/Main.java
@@ -11,20 +11,6 @@ public class Main {
             
             TreeDraw t = new TreeDraw(ds.getRoot(), ds.getWidth());
             t.show();
-            
-            /*
-            long startTime = System.currentTimeMillis();
-            Dataset ds = new Dataset(new File("./src/data_set.csv"), true);
-            long endTime = System.currentTimeMillis();
-            
-            System.out.println("Árbol creado");
-            System.out.println(ds.getSize() + "\t" + (endTime - startTime));
-            */
-            
-            //Data d = new Data(6.8, 23.5, 28.62, 3069, 36, 40, false);
-            //System.out.println("Antes: " + d.getLabel());
-            //d.setLabel(ds);
-            //System.out.println("Después: " + d.getLabel());
         }
         catch (FileNotFoundException e) {
             System.out.println("No se encontró el archivo.");
